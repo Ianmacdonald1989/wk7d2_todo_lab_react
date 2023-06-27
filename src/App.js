e8ee47f23fb413f518bcd7adaed57e4b7a660e77
Ianmacdonald1989 <127326567+Ianmacdonald1989@users.noreply.github.com>
2023-06-27 14:48:50 +0100

    react lab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    const [todos, setTodos] = useState([
+      {name: "Walk dog", isPriority: true},
+      {name: "Wash car", isPriority: false},
+      {name: "Wash dog", isPriority: true}
+    ]);
+
+    const [newTodo, setNewTodo] = useState("");
+
+    const todoNodes = todos.map((todo, index) => {
+      return(
+        <li key = {index} className={todo.isPriority ? "priority" : "not-priority"}>
+        <span>{todo.name}</span>
+        {todo.isPriority ? <span className='priority'>High Priority!</span> : <button onClick ={() => makePriority(index)}>Make Priority</button>}
+        </li>
+      );
+    });
+
+    const handleTodoInput = (event) => {
+      setNewTodo(event.target.value);
+    }
+
+    const saveNewTodo = (event) => {
+      event.preventDefault();
+      const copyTodos = [... todos];
+      copyTodos.push({name:newTodo, isPriority:false});
+      setTodos(copyTodos);
+      setNewTodo("");
+    }
+
+    const makePriority = (index) => {
+      const copyTodos = [...todos];
+      copyTodos[index].isPriority = true;
+      setTodos(copyTodos);
+    }
+
+    return (
+
+
+        <div className="App">
+          <h1>ToDo's</h1>
+          <hr></hr>
+          <ul>
+            {todoNodes}
+          </ul>
+          <form onSubmit={saveNewTodo}>
+            <label htmlFor='new-todo'>Add New ToDo:</label>
+            <input id='new-todo' type='text' value={newTodo} onChange={handleTodoInput}/>
+            <input type='submit' value='Save Todo'/>
+            </form>
+
+
     </div>
   );
 }
